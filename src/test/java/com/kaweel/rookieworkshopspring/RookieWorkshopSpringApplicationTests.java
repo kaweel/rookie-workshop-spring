bb02b3c694f6b23b1b721199135c1e1ce61b6b5a
Kawee Lertrungmongkol <lertrungmongkol@gmail.com>
2020-09-09 14:22:37 +0700

    initial

---
+++ src/test/java/com/kaweel/rookieworkshopspring/RookieWorkshopSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaweel.rookieworkshopspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RookieWorkshopSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
